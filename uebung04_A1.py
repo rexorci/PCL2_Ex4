9aa55183008a38df41f58cce4022cb655165a741
rexorci <christoph.t.weber@gmail.com>
2017-04-23 00:01:08 +0200

    added hashing function, deduplication by hashes in dictionary. output to file are now the HASHES of the 20 most common sentences (TODO: output sentence, performance?)

--- uebung04_A1.py
+++ uebung04_A1.py
@@ -4,52 +4,75 @@
 # Übung 4, Aufgabe 1
 # Autoren: Christoph Weber, Patrick Dueggelin
 # Matrikel-Nr.: 10-924-231, 14-704-738
-import argparse, glob
+import argparse
+import glob
+import operator
+import os
 from lxml import etree
 
 
 def getfreqwords(indir, outfile):
     # get all relevant files as a list
-    pattern = indir + '/SAC-Jahrbuch_1946_mul.xml'
+    pattern = indir + '/SAC-Jahrbuch_1864_mul.xml'
     # pattern = indir + '/SAC-Jahrbuch_[0-9][0-9][0-9][0-9]_mul.xml'
-    print('bla')
+
+    # we save the hash/string pairs in a file so we don't keep all the strings in memory
+    tmp_file_hash_plus_string = 'TMP_hash_plus_string.txt'
+
+    sentence_hashes = {}
     for file in glob.glob(pattern):
         tree = etree.parse(file)
         sentences = tree.xpath('/book/article/div/s')
-        print(len(sentences))
+
         for sentence in sentences:
-            __lemmatize_sentences(sentence)
-            print('--------------')
-
-        # code from lecture:
-        # with open(file) as infile, open(outfile, 'w') as outfile:
-        #     sentence_hashes = set()
-        #     for sentence in infile:
-        #         sentence_hash = hash(sentence)
-        #         if sentence_hash not in sentence_hashes:
-        #         sentence_hashes.add(sentence_hash)
-        #         trg.write(sentence)
+            __hash_sentence(sentence, sentence_hashes, tmp_file_hash_plus_string)
 
 
-    # get 20 most frequent sentences (with >= 6 tokens)
+    # sort the hashes by occurence (e.g. value)
+    count = 0
+    with open(outfile, 'w') as result_file:
+        for hash_sentence in sorted(sentence_hashes.items(), key=operator.itemgetter(1), reverse=True):
+            count += 1
+            if count > 20:
+                break
+            else:
+                result_file.writelines(str(hash_sentence[0]) + '\n')
+
 
     # write most frequent sentences to outfile
 
+    # remove the tmp file
+    if os.path.exists(tmp_file_hash_plus_string):
+        os.remove(tmp_file_hash_plus_string)
 
-def __lemmatize_sentences(sentence):
-    # create lemmatised sentences
-    for lemma in sentence.xpath('w/@lemma'):
-        print(lemma)
-    #yield sentence
+
+def __hash_sentence(sentence, sentence_hashes, tmp_file_hash_plus_string):
+    # concatenate the lemmata of the sentence and then return the hash value
+    sentence_string = ''
+    with open(tmp_file_hash_plus_string, 'ab') as tmp_file:
+        for lemma in sentence.xpath('w/@lemma'):
+            sentence_string += ' ' + lemma
+
+        # only sentences with minimum 6 tokens are relevant
+        if len(sentence.xpath('w/@lemma')) >= 6:
+            hash_sentence = hash(sentence_string)
+            if hash_sentence not in sentence_hashes:
+                sentence_hashes[hash_sentence] = 1
+                el = etree.Element('sentence', hash=str(hash_sentence), sentence=sentence_string)
+
+                tmp_file.write(etree.tostring(el))
+            else:
+                sentence_hashes[hash_sentence] += 1
+
 
 def main():
-    #Get the input and output filenames and encoding as commandline agruments
+    # Get the input and output filenames and encoding as commandline arguments
     parser = argparse.ArgumentParser(description='Find most frequent sentences.')
-    parser.add_argument('--indir','-i',
+    parser.add_argument('--indir', '-i',
                         dest='indir',
                         help='directory that contains the XML files')
 
-    parser.add_argument('--outfile','-o',
+    parser.add_argument('--outfile', '-o',
                         dest="outfile",
                         default='analysis_output.txt',
                         help='output file')
