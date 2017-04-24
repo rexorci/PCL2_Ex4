c1d96bcedd57ab693a60cf1af41697eec9aed84a
Patrick Dueggelin <patrick.dueggelin@live.de>
2017-04-24 21:57:42 +0200

    bugfixes
    -iterslice instead of list slice
    -opening and closing files for every read write, to save memory

--- uebung04_A2.py
+++ uebung04_A2.py
@@ -7,6 +7,7 @@
 import argparse
 import random
 
+from itertools import islice
 from lxml import etree
 from tempfile import mkstemp
 from shutil import move
@@ -24,59 +25,62 @@ def write_to_line(file_path, n, string):
     fh, abs_path = mkstemp()
     with open(abs_path,'w') as new_file:
         with open(file_path) as old_file:
-            i=0
+            i = 0
             for line in old_file:
                 if i == n:
                     new_file.write(string+"\n")
                 else:
                     new_file.write(line)
+                i += 1
     close(fh)
     #Remove original file
     remove(file_path)
     #Move new file
     move(abs_path, file_path)
 
+def write_to_newline(file_path, string):
+    file = open(file_path, 'w')
+    file.write(string + "\n")
+    file.close()
 
 def gettitles(infile_path, testfile_path, trainfile_path, k):
     """
     Gets a random sample of titles from the xml infile and writes it to the testfile,
-    The remaining titles are written to the trainfile. Using Algorithm R as described in:
+    The remaining titles are written to the trainfile.
+    Using an adapted version of Algorithm R as described in:
     Source: Vitter, Jeffrey S. (1 March 1985).
             Random sampling with a reservoir.
             ACM Transactions on Mathematical Software.
     """
 
     # We are only interested in elements that have the title tag.
+    # Ugly ass python 3
     titles = filter(
-        lambda event,elem: elem.tag == '{http://www.mediawiki.org/xml/export-0.10/}title',
+        lambda tuple: tuple[1].tag == '{http://www.mediawiki.org/xml/export-0.10/}title',
         etree.iterparse(infile_path))
 
-    testfile = testfile_path.open()
 
-    # The first k title elements are initially written to the testfile
+    # The first k title elements are initially written to the testfile.
+    # We discard the elements as soon as they were processed to reduce memory usage.
     i = 0
-    for event, elem in titles[:k]:
-        testfile.write(elem.text + "\n")
+    for event, elem in islice(titles, k):
+        write_to_newline(testfile_path, elem.text)
         i += 1
         elem.clear()  # discard the element
 
-    close(testfile)
-
-    trainfile = open(trainfile_path)
 
     # For the remaining elements, randomly replace an element of the testfile
     # with decreasing probability. If no element is replaced, the title is written
     # to the trainfile.
-    for event, elem in titles[k+1:]:
-        j = random.random(0, i)
-        i += 1
-        if j<k:
+    for event, elem in islice(titles, k+1, None):
+        j = random.randint(0, i)
+        if j < k:
             write_to_line(testfile_path, j, elem.text)
         else:
-            trainfile.write(elem.text + "\n")
+            write_to_newline(trainfile_path, elem.text)
+        i += 1
         elem.clear()  # discard the element
 
-    trainfile.close()
 
 
 def main():
@@ -100,7 +104,7 @@ def main():
 
     args = parser.parse_args()
 
-    gettitles(args.infile, args.testfile, args.trainfile, args.k)
+    gettitles(args.infile, args.testfile, args.trainfile, int(args.k))
 
 if __name__ == '__main__':
     main()
